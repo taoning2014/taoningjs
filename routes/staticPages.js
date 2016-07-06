ad0e0eaa3c73cd8af24fd86277c2032347194d0a
Tao Ning <ningtaous@gmail.com>
2016-07-05 22:39:57 -0400

    1, add timeline 2, modify header and footer

--- routes/staticPages.js
+++ routes/staticPages.js
@@ -4,4 +4,8 @@ router.get(/^\/(index(.html)?)?$/, function(req, res) {
   res.render('pages/index');
 });
 
+router.get('/timeline.html', function(req, res) {
+  res.render('pages/timeline');
+});
+
 module.exports = router;
