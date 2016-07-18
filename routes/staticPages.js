2bba3785d6764825b6170b5589afee2653b10ee6
Tao Ning <ningtaous@gmail.com>
2016-07-18 08:28:57 -0400

    mobile version set up

--- routes/staticPages.js
+++ routes/staticPages.js
@@ -1,7 +1,19 @@
 var router = require('express').Router();
 
+// Refer: http://www.hacksparrow.com/mobile-web-development-in-express-js-node-js.html
+function is_mobile(req) {
+  var ua = req.header('user-agent');
+  if (/mobile/i.test(ua)) return true;
+  else return false;
+};
+
 router.get(/^\/(index(.html)?)?$/, function(req, res) {
-  res.render('pages/index');
+  if (is_mobile(req)) {
+    res.render('mobile/index-mobi', {layout: 'layout-mobi'});
+  }
+  else {
+    res.render('pages/index');
+  }
 });
 
 router.get('/timeline.html', function(req, res) {
